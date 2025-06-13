35197e69746c11650eee1032944268215305bbd6
Peadar Byrne <peadarjb@gmail.com>
2025-06-13 22:36:27 +0100

    Installing yarn and npm packages and new index.html

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          // Override Ant Design Less variables if needed
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: [
+      '@ant-design/icons',
+      '@ant-design/pro-components',
+      '@ant-design/charts',
+    ],
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+});
