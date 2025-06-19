f0b784eb35eacd82a7962c514df57d2301e82717
Peadar Byrne <peadarjb@gmail.com>
2025-06-19 10:35:08 +0100

    feat: add custom React hooks for debouncing values and callbacks, enhancing performance and usability

---
+++ src/hooks/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Custom React hooks for the TII Flood Risk Dashboard
+ * Provides reusable logic with TypeScript support
+ */
+
+export { useDebounce } from './useDebounce';
+export { useThrottle } from './useThrottle';
+export { useLocalStorage } from './useLocalStorage';
+export { useAsync } from './useAsync';
+export { useEventListener } from './useEventListener';
+export { useResizeObserver } from './useResizeObserver';
+export { useInterval } from './useInterval';
+export { usePrevious } from './usePrevious';
+export { useUpdateEffect } from './useUpdateEffect';
+export { useMount } from './useMount';
+export { useUnmount } from './useUnmount';
+export { useToggle } from './useToggle';
+export { useCounter } from './useCounter';
+export { useHover } from './useHover';
+export { useClickOutside } from './useClickOutside';
