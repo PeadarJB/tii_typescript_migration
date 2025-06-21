ca868c8c0b15b46a3c482d3bfd585b0d5626458d
Peadar Byrne <peadarjb@gmail.com>
2025-06-21 12:21:29 +0100

    feat: add index file to export FilterPanel, StatisticsPanel, and SwipePanel components

---
+++ src/components/panels/index.ts
@@ -0,0 +1,5 @@
+// src/components/panels/index.ts
+
+export { default as FilterPanel } from './FilterPanel';
+export { default as StatisticsPanel } from './StatisticsPanel';
+export { default as SwipePanel } from './SwipePanel';
