071969b8b8b3d64b08c329af168e1134f3cc7466
Peadar Byrne <peadarjb@gmail.com>
2025-06-30 16:38:21 +0100

    feat: update index.ts to ensure proper export of PastFloodPage and PrecipitationPage

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as FutureHazardPage } from './FutureHazardPage';
 export { default as PastFloodPage } from './PastFloodPage';
+export { default as PrecipitationPage } from './PrecipitationPage';
